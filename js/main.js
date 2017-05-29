bbfb023e516d4f9ad0d5deb611adf32b429e6af3
AdrianaHY <adriana.herya@gmail.com>
2017-05-29 15:18:29 -0500

    Realicé funcionalidad básica del carrusel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-
+// funciones para carrusel
+$(document).ready(function(){
+  //para que escondas todas las imágenes excepto la primera
+  $("img").hide();
+  $("#imagenUno").show();
+  //para que cada punto muestre una imagenUno
+  //span1
+  $(".dot1").click(function(){
+    $("img").hide();
+    $("#imagenUno").show();
+  });
+  //span2
+  $(".dot2").click(function(){
+    $("img").hide();
+    $("#imagenDos").show();
+  });
+  //span3
+  $(".dot3").click(function(){
+    $("img").hide();
+    $("#imagenTres").show();
+  });
+  //span4
+  $(".dot4").click(function(){
+    $("img").hide();
+    $("#imagenCuatro").show();
+  });
+  //span5
+  $(".dot5").click(function(){
+    $("img").hide();
+    $("#imagenCinco").show();
+  });
+});
